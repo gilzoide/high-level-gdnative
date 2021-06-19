fa02710acb45aba3ff6de6ef4e8df51ae4c80c37
gilzoide <gilzoide@gmail.com>
2021-06-19 01:20:51 -0300

    Fix remove docs folder

--- docs/search/files_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['hgdn_2eh_34',['hgdn.h',['../hgdn_8h.html',1,'']]]
-];
--- docs/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['high_20level_20gdnative_20c_2fc_2b_2b_20api_20_28hgdn_29_63',['High level GDNative C/C++ API (HGDN)',['../index.html',1,'']]]
-];
